60d2be8cb0d1b4c42dc79e1ac7fd53e92798d2aa
Luc033 <lucasrm33.contato@gmail.com>
2025-08-27 19:22:15 -0300

    Creating asset folder Inserting all assets Creating the menu and const file

---
+++ code/Const.py
@@ -0,0 +1,14 @@
+# Contantes das cores utilizadas no jogo
+COLOR_ORANGE = (25, 128, 0)
+COLOR_WHITE = (255, 255, 255)
+
+# Constantes dos textos de opções do Menu
+MENU_OPTION = ('NEW GAME 1P',
+               'NEW GAME 2P - COOPERATIVE',
+               'NEW GAME 2P - COMPTETITIVE',
+               'SCORE',
+               'EXIT')
+
+# Constantes da Altura e Largura da tela do jogo
+WIN_WIDTH = 576
+WIN_HEIGHT = 324
